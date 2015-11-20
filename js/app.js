9d006b846a76bd02786c758c48a3191dfb1fbb86
Julian Bossiere <julianbossiere@gmail.com>
2015-11-19 23:31:07 -0800

    blah

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Initialize app
+var app = angular.module('myApp', ['ui.router', 'ui.material']);
+
+$.backstretch("img/skyline.png");
+
+// Configure app
+app.config(function($stateProvider){
+	$stateProvider
+		.state('landing', {
+			url: '/',
+			templateUrl: "index.html",
+			controller: "HomeController"
+		})
+		.state('about', {
+			url: '/about',
+			templateUrl: 'templates/about.html',
+			controller: "AboutController"
+		})
+		.state('contact', {
+			url: '/contact',
+			templateUrl: 'templates/contact.html',
+			controller: 'ContactController'
+		})
+});
+
+app.controller('HomeController', function($scope, $http){
+
+});
+
+app.controller('AboutController', function($scope, $http){
+	$scope.name = 'Julian Bossiere';
+});
+
+app.controller('ContactController', function($scope, $http){
+	$scope.email = "julianbossiere@gmail.com";
+});
