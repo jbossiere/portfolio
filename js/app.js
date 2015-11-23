6351a0da400d44212c95379d4d02b58e8a429231
jgb93 <jgb93@D-172-28-35-190.dhcp4.washington.edu>
2015-11-23 05:16:14 -0800

    trying to figure out errors

--- js/app.js
+++ js/app.js
@@ -12,51 +12,23 @@ app.config(function($stateProvider){
 		.state('about', {
 			url: '/about',
 			templateUrl: 'templates/about.html',
-			controller: "AboutController"
 		})
 		.state('contact', {
 			url: '/contact',
 			templateUrl: 'templates/contact.html',
-			controller: 'ContactController'
 		})
 });
 
 // Creates a controller for the home fragment/template, then parses through a csv file to get data
 // to show on the page
-app.controller('HomeController', function($scope, $http){
-
-	Papa.parse("../data/home.csv", {
-		download: true,
-		header: true,
-		complete: function(results) {
-			console.log(results);
-			$scope.projects = results.data;
-			// for (i  = 0; i < results.data.length; i++) {
-			// 	Papa.parse(results.data[i].tags, {
-			// 		complete: function(results) {
-			// 			console.log(results)
-			// 			console.log(results.data);
-			// 		}
-			// 	});
-			// };
-		}
+app.controller('HomeController', function($scope){
+	angular.element(document).ready(function() {
+		Papa.parse("../data/home.csv", {
+			download: true,
+			header: true,
+			complete: function(results) {
+				$scope.projects = results.data;
+			}
+		});
 	});
-
-
-	// $scope.animation = function(project) {
-	// 	$attr('src', 'project.imgUrl')
-	// };
-
-
 });
-
-// Creates a controller for the about fragment/template
-app.controller('AboutController', function($scope, $http){
-	$scope.name = 'Julian Bossiere';
-});
-
-// Creates a controller for the contact fragment/template
-app.controller('ContactController', function($scope, $http){
-	$scope.email = "julianbossiere@gmail.com";
-});
-
