b3c7dbd71cded4cc37268e17f0f966acda1d3263
Julian Bossiere <julianbossiere@gmail.com>
2015-11-24 14:05:32 -0800

    added comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Initializes the angular app
 var app = angular.module('myApp', ['ui.router', 'ui.materialize']);
 
-// Configure app to give separate controllers for the various html fragment/templates
+// Configure app to give separate controller for home, and separate templateUrls for the various html fragment/templates
 app.config(function($stateProvider){
 	$stateProvider
 		.state('home', {
@@ -22,13 +22,11 @@ app.config(function($stateProvider){
 // Creates a controller for the home fragment/template, then parses through a csv file to get data
 // to show on the page
 app.controller('HomeController', function($scope){
-	angular.element(document).ready(function() {
-		Papa.parse("./data/home.csv", {
-			download: true,
-			header: true,
-			complete: function(results) {
-				$scope.projects = results.data;
-			}
-		});
+	Papa.parse("data/home.csv", {
+		download: true,
+		header: true,
+		complete: function(results) {
+			$scope.projects = results.data;
+		}
 	});
 });
